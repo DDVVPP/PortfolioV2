99330aa0c1135055605e34adb9b73e110db4d55f
DDVVPP <darshinvp@gmail.com>
2024-07-16 11:59:58 -0700

    update: added real links to navbar items

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,39 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import githubLogo from '@/public/github-mark.svg';
+import linkedInLogo from '@/public/In-White-14.png';
+
 const Navbar = () => {
   return (
     <section className="flex flex-col gap-y-8">
       <section className="flex flex-col">
-        <h1>Darshin Van Parijs</h1>
-        <div className="flex gap-x-2">
-          <p>Resume</p>
-          <p>linkedInIcon</p>
-          <p>githubIcon</p>
+        <div className="text-3xl">
+          <h1>Darshin</h1>
+          <h1>Van Parijs</h1>
         </div>
-        <p>darshinvp@gmail.com</p>
+        <div className="mt-4 flex gap-x-2">
+          <a href="/resume-2024.pdf" target="_blank">
+            <p>Resume</p>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/DarshinVanParijs/"
+            target="_blank"
+          >
+            <Image priority src={linkedInLogo} alt="LogoLight" width={20} />
+          </a>
+          <a href="https://github.com/DDVVPP" target="_blank">
+            <Image priority src={githubLogo} alt="LogoLight" width={24} />
+          </a>
+        </div>
+        <a href="mailto:darshinvp@gmail.com">darshinvp@gmail.com</a>
       </section>
-      <section>
-        <p>About</p>
-        <p>Work</p>
-        <p>Projects</p>
-        <p>Hobbies</p>
+
+      <section className="flex flex-col gap-y-4">
+        <Link href="/about">About</Link>
+        <Link href="/work">Work</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/hobbies">Hobbies</Link>
       </section>
     </section>
   );
