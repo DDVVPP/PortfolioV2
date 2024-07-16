d3f122e87d196293c1f0bfd7755581f088bb39fe
DDVVPP <darshinvp@gmail.com>
2024-07-15 18:51:06 -0700

    wip: added Navbar skeleton

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,23 @@
 const Navbar = () => {
-  return <div>Left Navbar</div>;
+  return (
+    <section className="flex flex-col gap-y-8">
+      <section className="flex flex-col">
+        <h1>Darshin Van Parijs</h1>
+        <div className="flex gap-x-2">
+          <p>Resume</p>
+          <p>linkedInIcon</p>
+          <p>githubIcon</p>
+        </div>
+        <p>darshinvp@gmail.com</p>
+      </section>
+      <section>
+        <p>About</p>
+        <p>Work</p>
+        <p>Projects</p>
+        <p>Hobbies</p>
+      </section>
+    </section>
+  );
 };
 
 export default Navbar;
