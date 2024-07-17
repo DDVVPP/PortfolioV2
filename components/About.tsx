9389caad9e3bd3d9cad522af50fa6dac05b660b1
DDVVPP <darshinvp@gmail.com>
2024-07-17 13:29:35 -0700

    style: added highlight text to about

--- components/About.tsx
+++ components/About.tsx
@@ -23,17 +23,23 @@ const About = () => {
       </div>
       <div className="w-3/4 justify-center max-md:text-sm">
         <p className="text-center font-light text-slate-300">
-          Hi! I am an Architectural Designer turned Software Developer who lives
-          in Los Angeles, California and likes to eat delicious food, workout,
+          Hi! I am an Architectural Designer turned{' '}
+          <span className="text-red-400">Software Developer</span> who lives in
+          Los Angeles, California and likes to eat delicious food, workout,
           hike, and dance Brazilian Zouk (a latin partner dance). <br />
-          <br />I have experience building applications in Typescript,
-          Javascript, React, Next.js, PosGreSQL, and GraphQL using agile
-          methodologies. <br />
+          <br />I have experience building applications in{' '}
+          <span className="text-red-400">
+            Typescript, Javascript, React, Next.js, PosGreSQL, and GraphQL using
+            agile methodologies.
+          </span>
           <br />
-          My previous career in architecture has provided me with valuable
-          skills, such as the implementation of the iterative process of design,
-          team collaboration and coordination, and analysis and problem solving,
-          which I have carried with me into my software engineer career.
+          <br />
+          My previous career in{' '}
+          <span className="text-red-400">architecture</span> has provided me
+          with valuable skills, such as the implementation of the iterative
+          process of design, team collaboration and coordination, and analysis
+          and problem solving, which I have carried with me into my software
+          engineer career.
           <br />
           <br />I enjoy coding on the frontend - natural capacity for - visuals
           testing- immediate- design. Enjoy backend - manage and be aware of
