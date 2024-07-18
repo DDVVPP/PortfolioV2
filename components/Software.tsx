ea706388f678504b3128fe65c2008ed97691de93
DDVVPP <darshinvp@gmail.com>
2024-07-18 12:38:45 -0700

    update: updated breakpoint behavior in components

--- components/Software.tsx
+++ components/Software.tsx
@@ -6,21 +6,21 @@ import { workItems } from '@/lib/constants';
 const Software = () => {
   return (
     <motion.section
-      className="mt-10 flex flex-col"
+      className=""
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.8 }}
     >
-      <div className="flex flex-col gap-y-8">
+      <div className="flex flex-col gap-y-8 max-md:gap-y-0">
         {workItems.map((item) => {
           return (
             <a
               key={item.company}
               href={item.website}
               target="_blank"
-              className="rounded-lg p-4 pl-3 duration-300 hover:bg-slate-900"
+              className="rounded-lg p-4 pl-3 duration-300 hover:bg-slate-900 max-md:px-3"
             >
-              <div className="flex gap-x-4 text-sm text-slate-300">
+              <div className="flex gap-x-4 text-sm text-slate-300 max-md:flex-col max-md:gap-x-0">
                 <p className="min-w-44 uppercase text-slate-300">{item.date}</p>
                 <div className="flex flex-col">
                   <h6 className="mb-1 font-medium uppercase text-slate-100">
