ac8cdea2865a1648fce31146a407f86dcd742cca
DDVVPP <darshinvp@gmail.com>
2024-07-17 18:59:26 -0700

    update: added website links to engineering experience

--- components/Software.tsx
+++ components/Software.tsx
@@ -6,7 +6,7 @@ import { workItems } from '@/lib/constants';
 const Software = () => {
   return (
     <motion.section
-      className="ml-2 mt-10 flex flex-col"
+      className="mt-10 flex flex-col"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.8 }}
@@ -14,19 +14,27 @@ const Software = () => {
       <div className="flex flex-col gap-y-8">
         {workItems.map((item) => {
           return (
-            <div
+            <a
               key={item.company}
-              className="flex gap-x-4 text-sm text-slate-300"
+              href={item.website}
+              target="_blank"
+              className="rounded-lg p-4 pl-3 duration-300 hover:bg-slate-900"
             >
-              <p className="min-w-44 uppercase text-slate-300">{item.date}</p>
-              <div className="flex flex-col">
-                <h6 className="mb-1 font-medium uppercase text-slate-100">
-                  {item.company}
-                </h6>
-                <h6 className="mb-1 font-normal text-slate-100">{item.role}</h6>
-                <p className="font-light text-slate-300">{item.description}</p>
+              <div className="flex gap-x-4 text-sm text-slate-300">
+                <p className="min-w-44 uppercase text-slate-300">{item.date}</p>
+                <div className="flex flex-col">
+                  <h6 className="mb-1 font-medium uppercase text-slate-100">
+                    {item.company}
+                  </h6>
+                  <h6 className="mb-1 font-normal text-slate-100">
+                    {item.role}
+                  </h6>
+                  <p className="font-light text-slate-300">
+                    {item.description}
+                  </p>
+                </div>
               </div>
-            </div>
+            </a>
           );
         })}
       </div>
