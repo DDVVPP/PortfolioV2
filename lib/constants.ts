762458f04766e97d914ce0050f38dd17a66ae887
DDVVPP <darshinvp@gmail.com>
2024-07-19 13:53:54 -0700

    update: updated architecture work text

--- lib/constants.ts
+++ lib/constants.ts
@@ -109,7 +109,7 @@ export const architectureWorkItems = [
     date: 'Sept 2017 - Dec 2018',
     company: 'Bade Stageberg Cox Architecture',
     role: 'Project Architect / Architectural Designer',
-    description: `Supervised multiple construction phases for the relocation of Artists Space, an iconic NYC art gallery – Managed the renovation of a summer kitchen highly tailored to high-profile client’s requirements – Implemented diagrams, detailed sketches, and revised shop drawings in AutoCad and Illustrator – Illustrated concepts as 3D models and renderings in Sketch-up, V-ray, Rhinoceros and Photoshop.`,
+    description: `Supervised multiple construction phases for the relocation of Artists Space, an iconic NYC art gallery – Managed the renovation of a summer kitchen highly tailored to high-profile client’s requirements – Implemented diagrams, detailed sketches, and revised shop drawings – Illustrated concepts as 3D models and renderings – Involved in contractor, client, and consultant correspondence and coordination.`,
     website: 'https://www.bscarchitecture.com/',
     tags: [
       'AutoCad',
@@ -124,7 +124,7 @@ export const architectureWorkItems = [
     date: 'Oct 2015 - Sept 2017',
     company: "O'Neill Rose Architects",
     role: 'Project Architect / Architectural Designer',
-    description: `Managed multiple phases of single-family NYC residences – Developed and generated detailed drawings and sketches in AutoCad – Revised shop drawings in Adobe Illustrator – Generated 3D models, diagrams, and renderings in Sketch-up, V-ray, Rhinoceros, and Adobe Photoshop`,
+    description: `Managed multiple phases of single-family NYC residences – Developed and generated detailed drawings and sketches – Revised shop drawings – Generated 3D models, diagrams, and renderings – Involved in contractor, client, and consultant correspondence and coordination.`,
     website: 'https://www.oneillrosearchitects.com/',
     tags: [
       'AutoCad',
@@ -140,7 +140,7 @@ export const architectureWorkItems = [
     company: 'Audrey Matlock Architect',
     role: 'Project Architect / Architectural Designer',
     description: `Managed the Construction Administration phase for a single-family NYC apartment
-– Organized and coordinated interior design work of an award-winning Hamptons residence – Developed design drawings and detailed drawings in Vectorworks, Adobe Illustrator, and Photoshop – Generated 3D models and renderings in Rhinoceros, and V-ray`,
+– Organized and coordinated interior design work of an award-winning Hamptons residence – Developed design drawings and detailed drawings – Generated 3D models and renderings.`,
     website: 'http://www.audreymatlock.com/',
     tags: ['Vectorworks', 'Illustrator', 'Photoshop', 'Vray', 'Rhinoceros'],
   },
@@ -149,7 +149,7 @@ export const architectureWorkItems = [
     company: 'Lauren Rubin Architecture',
     role: 'Architectural Designer',
     description: `Assisted in the construction administration phase of multiple single family residences
-– Organized and coordinated interior design work for an advertising company and a single-family residence – Generated drawings for furniture placement with AutoCad – Generated 3D concept renderings of each room with Sketchup, Adobe Photoshop, and AutoCad – Created all phases of document sets for two single-family residences with AutoCad`,
+– Organized and coordinated interior design work for an advertising company and a single-family residence – Generated drawings for furniture placement – Generated 3D concept renderings of each room with – Created all phases of document sets for two single-family residences.`,
     website: 'https://laurenrubinarchitecture.com/',
     tags: ['AutoCad', 'Photoshop', 'Sketchup'],
   },
@@ -157,7 +157,8 @@ export const architectureWorkItems = [
     date: 'Jul 2009 - Apr 2010',
     company: 'Brillembourg Architects',
     role: 'Junior Architectural Designer',
-    description: '',
+    description:
+      'Assisted in the development of a design proposal for a housing complex/hotel in Venezuela with 3D models, renderings, and 2D drawings – Assisted in all phases of design, development, and construction for a one bedroom Manhattan residential renovation, including the custom design of a dining table and lamp – Revised shop drawings and implemented detailed sketches.',
     website: 'https://www.carlosbrillembourgarchitects.com/',
     tags: ['AutoCad', 'Photoshop', 'Sketchup'],
   },
@@ -165,8 +166,16 @@ export const architectureWorkItems = [
     date: 'Sep 2007 - April 2009',
     company: 'Garrison Architects',
     role: 'Junior Architectural Designer',
-    description: '',
+    description:
+      'Managed the Grand Army Plaza International Competition submission in Brooklyn, NY – Contributed to concept ideas, 3D models, renderings, and design drawings – Assisted in all phases of design and development for the US Consulate Residence in Samoa – Generated 3D Models, renderings, schematic design, design development, and construction documents',
     website: 'https://www.garrisonarchitects.com/',
-    tags: ['AutoCad', 'Photoshop', 'Vray', 'Maxwell Render'],
+    tags: [
+      'AutoCad',
+      'Photoshop',
+      'Illustrator',
+      'Sketchup',
+      'Vray',
+      'Maxwell Render',
+    ],
   },
 ];
