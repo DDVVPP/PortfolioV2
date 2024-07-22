c15c3a9d172cd8f5ee35b7e3b0519e4e4194d084
DDVVPP <darshinvp@gmail.com>
2024-07-22 14:16:15 -0700

    wip: images with animation

--- components/Art.tsx
+++ components/Art.tsx
@@ -29,50 +29,30 @@ const Art = () => {
   };
 
   return (
-    <section className="flex flex-wrap gap-2">
-      {artImages.map((image, idx) => {
+    <motion.section
+      className="flex flex-wrap gap-2"
+      variants={container}
+      initial="hidden"
+      animate="show"
+    >
+      {artImages.map((image) => {
         return (
-          <div
-            key={idx}
+          <motion.div
+            key={image.altText}
+            variants={item}
             className="relative size-24 shrink-0 overflow-hidden align-middle max-md:size-24"
           >
-            <p className="z-50 text-xl text-red-500">{idx}</p>
             <Image
+              key={image.altText}
               alt={image.altText}
               src={image.src}
               fill
               className="rounded-xl object-cover"
-              key={idx}
             />
-          </div>
+          </motion.div>
         );
       })}
-    </section>
-    // <motion.section
-    //   className="flex flex-wrap gap-2"
-    //   variants={container}
-    //   initial="hidden"
-    //   animate="show"
-    // >
-    //   {artImages.map((image, idx) => {
-    //     return (
-    //       <motion.div
-    //         key={idx}
-    //         variants={item}
-    //         className="relative size-24 shrink-0 overflow-hidden align-middle max-md:size-24"
-    //       >
-    //         <p className="text-xl text-red-500">{idx}</p>
-    //         <Image
-    //           alt={image.altText}
-    //           src={image.src}
-    //           fill
-    //           className="rounded-xl object-cover"
-    //           key={idx}
-    //         />
-    //       </motion.div>
-    //     );
-    //   })}
-    // </motion.section>
+    </motion.section>
   );
 };
 
