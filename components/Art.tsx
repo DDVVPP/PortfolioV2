527b734ee6135f8b06578cf76b58a16b5c7e04a0
DDVVPP <darshinvp@gmail.com>
2024-07-22 13:28:41 -0700

    wip: images

--- components/Art.tsx
+++ components/Art.tsx
@@ -5,28 +5,74 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 
 const Art = () => {
+  const container = {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const item = {
+    hidden: {
+      opacity: 0,
+      scale: 0,
+    },
+    show: {
+      opacity: 1,
+      scale: 1,
+    },
+  };
+
   return (
-    <motion.section
-      className="flex flex-wrap justify-between gap-2"
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 0.8 }}
-    >
-      {artImages.map((image) => (
-        <div
-          key={image.altText}
-          className="relative size-24 shrink-0 overflow-hidden align-middle max-md:size-24"
-        >
-          <Image
-            priority
-            alt={image.altText}
-            src={image.src}
-            fill
-            className="rounded-xl object-cover"
-          />
-        </div>
-      ))}
-    </motion.section>
+    <section className="flex flex-wrap gap-2">
+      {artImages.map((image, idx) => {
+        return (
+          <div
+            key={idx}
+            className="relative size-24 shrink-0 overflow-hidden align-middle max-md:size-24"
+          >
+            <p className="z-50 text-xl text-red-500">{idx}</p>
+            <Image
+              alt={image.altText}
+              src={image.src}
+              fill
+              className="rounded-xl object-cover"
+              key={idx}
+            />
+          </div>
+        );
+      })}
+    </section>
+    // <motion.section
+    //   className="flex flex-wrap gap-2"
+    //   variants={container}
+    //   initial="hidden"
+    //   animate="show"
+    // >
+    //   {artImages.map((image, idx) => {
+    //     return (
+    //       <motion.div
+    //         key={idx}
+    //         variants={item}
+    //         className="relative size-24 shrink-0 overflow-hidden align-middle max-md:size-24"
+    //       >
+    //         <p className="text-xl text-red-500">{idx}</p>
+    //         <Image
+    //           alt={image.altText}
+    //           src={image.src}
+    //           fill
+    //           className="rounded-xl object-cover"
+    //           key={idx}
+    //         />
+    //       </motion.div>
+    //     );
+    //   })}
+    // </motion.section>
   );
 };
 
