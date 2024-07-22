5108d5eb9f692897cb767402ac6b7d46001be6b3
DDVVPP <darshinvp@gmail.com>
2024-07-22 09:22:24 -0700

    feat: added images to constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,60 @@
+import {
+  AbstractFlower,
+  ArmadilloTails,
+  AbstractFlowerProcess,
+  Agape,
+  BeehiveVase,
+  BenchLines,
+  Beowolf,
+  BleedingBlues,
+  BubbleVase1,
+  BubbleVase2,
+  Clothesline,
+  ColoredTiles,
+  ColorSplats,
+  ColorSplats2,
+  ColorSplatsDots,
+  Crow,
+  CrowProcess,
+  CrowsSelfPortrait,
+  Curlew,
+  Day2Spider,
+  Day4PipeWorms,
+  Day5CityMoon,
+  Flowers,
+  FlyingGecko,
+  GlazedPlates,
+  FlyingGeckoProcess,
+  HatGlazeware2,
+  HeartSplatters,
+  HouseLines,
+  Jellyfish,
+  Jellyfish2,
+  KingCrownDiamonds,
+  LandscapeHand,
+  LittleMan,
+  LittleManGlazed,
+  LoopyManGlazed3,
+  LoopyManProcess,
+  ManLinesPaint,
+  ManWithWeeds,
+  Mask1,
+  Mask2,
+  MirrorMirror,
+  PhonesRamen,
+  QuizzicalFigure,
+  RamenGlazeware1,
+  RamenGlazeware2,
+  SelfPortraitPaint,
+  SpaceMan,
+  SpaghettiPeople,
+  SpillingOver,
+  Toothbrush,
+  TreesAndMountains,
+  UndulatingPlanter,
+  WormVase,
+} from '@/public/art';
+
 export const navbarItems = [
   {
     route: '/about',
@@ -177,5 +234,226 @@ export const architectureWorkItems = [
       'Vray',
       'Maxwell Render',
     ],
+  },
+];
+
+export const artImages = [
+  {
+    src: ArmadilloTails,
+    altText: 'armadillo-tails',
+  },
+  {
+    src: BenchLines,
+    altText: 'bench-lines',
+  },
+  {
+    src: Clothesline,
+    altText: 'clothesline',
+  },
+  {
+    src: HouseLines,
+    altText: 'house-lines',
+  },
+  {
+    src: ManWithWeeds,
+    altText: 'man-with-weeds',
+  },
+  {
+    src: SpaceMan,
+    altText: 'space-man',
+  },
+  {
+    src: SpaghettiPeople,
+    altText: 'spaghetti-people',
+  },
+  {
+    src: CrowsSelfPortrait,
+    altText: 'crows-self-portrait',
+  },
+  {
+    src: SpillingOver,
+    altText: 'spilling-over',
+  },
+  {
+    src: Beowolf,
+    altText: 'beowolf',
+  },
+  {
+    src: KingCrownDiamonds,
+    altText: 'king-crown-diamonds',
+  },
+  {
+    src: Flowers,
+    altText: 'flowers',
+  },
+  {
+    src: BleedingBlues,
+    altText: 'bleeding-blues',
+  },
+  {
+    src: HeartSplatters,
+    altText: 'heart-splatters',
+  },
+  {
+    src: Day2Spider,
+    altText: 'day-2-spider',
+  },
+  {
+    src: Day4PipeWorms,
+    altText: 'day-4-pipe-worms',
+  },
+  {
+    src: Day5CityMoon,
+    altText: 'day-5-city-moon',
+  },
+  {
+    src: MirrorMirror,
+    altText: 'mirror-mirror',
+  },
+  {
+    src: Toothbrush,
+    altText: 'toothbrush',
+  },
+  {
+    src: TreesAndMountains,
+    altText: 'trees-and-mountains',
+  },
+  {
+    src: Curlew,
+    altText: 'curlew',
+  },
+  {
+    src: LandscapeHand,
+    altText: 'landscape-hand',
+  },
+  {
+    src: PhonesRamen,
+    altText: 'phones-ramen',
+  },
+  {
+    src: CrowProcess,
+    altText: 'crow-process',
+  },
+  {
+    src: Crow,
+    altText: 'crow',
+  },
+  {
+    src: AbstractFlowerProcess,
+    altText: 'abstract-flower-process',
+  },
+  {
+    src: AbstractFlower,
+    altText: 'abstract-flower',
+  },
+  {
+    src: Jellyfish,
+    altText: 'jellyfish',
+  },
+  {
+    src: Jellyfish2,
+    altText: 'jellyfish-2',
+  },
+  {
+    src: FlyingGeckoProcess,
+    altText: 'flying-gecko-process',
+  },
+  {
+    src: FlyingGecko,
+    altText: 'flying-gecko',
+  },
+  {
+    src: SelfPortraitPaint,
+    altText: 'self-portrait-paint',
+  },
+  {
+    src: ManLinesPaint,
+    altText: 'man-lines-paint',
+  },
+  {
+    src: ColoredTiles,
+    altText: 'colored-tiles',
+  },
+  {
+    src: ColorSplatsDots,
+    altText: 'color-splats-dots',
+  },
+  {
+    src: ColorSplats,
+    altText: 'color-splats',
+  },
+  {
+    src: ColorSplats2,
+    altText: 'color-splats-2',
+  },
+
+  {
+    src: GlazedPlates,
+    altText: 'glazed-plates',
+  },
+  {
+    src: HatGlazeware2,
+    altText: 'hat-glazeware-2',
+  },
+  {
+    src: BeehiveVase,
+    altText: 'beehive-vase',
+  },
+  {
+    src: RamenGlazeware1,
+    altText: 'ramen-glazeware-1',
+  },
+  {
+    src: RamenGlazeware2,
+    altText: 'ramen-glazeware-2',
+  },
+  {
+    src: BubbleVase1,
+    altText: 'bubble-vase-1',
+  },
+  {
+    src: BubbleVase2,
+    altText: 'bubble-vase-2',
+  },
+  {
+    src: QuizzicalFigure,
+    altText: 'quizzical-figure',
+  },
+
+  {
+    src: UndulatingPlanter,
+    altText: 'undulating-planter',
+  },
+  {
+    src: WormVase,
+    altText: 'worm-vase',
+  },
+  {
+    src: Mask1,
+    altText: 'mask-1',
+  },
+  {
+    src: Mask2,
+    altText: 'mask-2',
+  },
+  {
+    src: Agape,
+    altText: 'agape',
+  },
+  {
+    src: LittleMan,
+    altText: 'little-man',
+  },
+  {
+    src: LittleManGlazed,
+    altText: 'little-man-glazed',
+  },
+  {
+    src: LoopyManProcess,
+    altText: 'loopy-man-process',
+  },
+  {
+    src: LoopyManGlazed3,
+    altText: 'loopy-man-glazed-3',
   },
 ];
