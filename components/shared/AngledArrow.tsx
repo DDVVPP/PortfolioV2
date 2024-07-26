9cd03c04afb50d106e319e32234cf1e4ac489d7e
DDVVPP <darshinvp@gmail.com>
2024-07-25 17:23:41 -0700

    update: added angled arrow to work links

---
+++ components/shared/AngledArrow.tsx
@@ -0,0 +1,20 @@
+const AngledArrow = (props: any) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      viewBox="0 0 22 26"
+      fill="none"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M7 7h10v10" />
+      <path d="M7 17 17 7" />
+    </svg>
+  );
+};
+
+export default AngledArrow;
