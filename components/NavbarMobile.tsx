29b1b6a0ae6e9c2bd26dea2d8036db7063eaa7cc
DDVVPP <darshinvp@gmail.com>
2024-07-28 14:14:36 -0700

    style: updated navbar mobile styles

--- components/NavbarMobile.tsx
+++ components/NavbarMobile.tsx
@@ -12,18 +12,18 @@ const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <section className="sticky flex flex-col gap-y-6">
+    <section className="sticky flex flex-col gap-y-10">
       <section className="flex items-center justify-between">
         <Link
           href="/about"
-          className="text-base font-bold text-slate-400 duration-300 hover:text-red-300"
+          className="text-4xl font-bold text-slate-400 duration-300 hover:text-red-300 max-sm:text-base"
         >
           <h1>Darshin</h1>
           <h1>Van Parijs</h1>
         </Link>
 
-        <div className="mt-0">
-          <div className="mb-0.5 flex items-center justify-end gap-x-[18px] text-xs font-light text-slate-300">
+        <div className="mt-0 flex flex-col gap-y-1">
+          <div className="mb-0.5 flex items-center justify-end gap-x-[18px] font-light text-slate-300 max-sm:text-xs">
             <a
               href="https://www.linkedin.com/in/DarshinVanParijs/"
               target="_blank"
@@ -44,7 +44,7 @@ const Navbar = () => {
           <div className="flex justify-end">
             <a
               href="mailto:darshinvp@gmail.com"
-              className="rounded-md border border-slate-500 p-1 text-end text-xs font-light text-slate-300"
+              className="rounded-md border border-slate-500 p-1 text-end font-light text-slate-300 max-sm:text-xs"
             >
               darshinvp@gmail.com
             </a>
@@ -52,7 +52,7 @@ const Navbar = () => {
         </div>
       </section>
 
-      <section className="flex justify-between text-base font-light text-slate-400 max-md:text-sm">
+      <section className="flex justify-between text-base font-light text-slate-400 max-sm:text-sm">
         {navbarItems.map((item) => {
           const isSelected = pathname.includes(item.route);
           return (
