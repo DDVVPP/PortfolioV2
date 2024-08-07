0a81c119e32820a8a0a74a41ceb61687bb4d47dc
DDVVPP <darshinvp@gmail.com>
2024-08-06 19:31:07 -0700

    update: disabled gitNote project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,7 +17,36 @@ const Projects = () => {
     >
       <div className="mx-10 my-32 flex flex-col gap-y-8 max-md:gap-y-6">
         {projects.map((project) => {
-          return (
+          return project.isDisabled ? (
+            <div
+              key={project.title}
+              className=" flex gap-x-8 rounded-lg  p-4 pl-3  max-md:flex-col max-md:gap-y-4 max-md:px-3"
+            >
+              <div className="relative w-80 min-w-20 opacity-40 max-md:w-full">
+                <Image
+                  alt={project.altText}
+                  src={project.image}
+                  className="rounded-lg"
+                  placeholder="blur"
+                />
+              </div>
+              <div className="mb-3 flex w-3/4 max-w-[1000px] flex-col justify-between max-md:-order-1 max-md:mb-2 max-md:w-full">
+                <div className="flex gap-x-0.5">
+                  <h1 className="animate-pulse text-wrap text-sm font-medium uppercase text-slate-100">
+                    {project.title} - COMING SOON!
+                  </h1>
+                </div>
+                <h3 className="text-sm font-light text-slate-300 opacity-40">
+                  {project.description}
+                </h3>
+                <div className="mt-4 flex flex-wrap gap-x-3 gap-y-2 opacity-40">
+                  {project.tags.map((tag) => {
+                    return <Tag key={tag} text={tag} />;
+                  })}
+                </div>
+              </div>
+            </div>
+          ) : (
             <Link
               key={project.title}
               href={project.route}
@@ -27,7 +56,7 @@ const Projects = () => {
                 <Image
                   alt={project.altText}
                   src={project.image}
-                  className="rounded-xl"
+                  className="rounded-lg"
                   placeholder="blur"
                 />
               </div>
