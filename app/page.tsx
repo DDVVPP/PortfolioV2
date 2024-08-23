5e6daf431d87047aab74e8cf5efb084e10ab2b4d
DDVVPP <darshinvp@gmail.com>
2024-08-23 00:52:28 -0700

    update: updated and added footer to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,14 @@ import Home from '@/components/Home';
 
 const HomeWrapper = () => {
   return (
-    <section className="w-full bg-slate-900">
-      <Home />
-    </section>
+    <>
+      <section className="w-full bg-slate-900">
+        <Home />
+      </section>
+      <footer className="mb-0 flex w-full justify-center bg-slate-900 py-5 pr-[51px] align-bottom text-xs font-light text-slate-400 max-md:justify-center max-md:text-wrap max-md:px-12 max-md:text-center">
+        © Designed & Built by Darshin Van Parijs & Deployed with Vercel
+      </footer>
+    </>
   );
 };
 
