0006fc0174cac2aa97e92ee433ffb73c5b801e41
DDVVPP <darshinvp@gmail.com>
2024-08-23 15:59:30 -0700

    fix: height responsiveness of home page images in large screens, and horizontal mobile screens

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -21,12 +21,12 @@ const Contact = () => {
   };
 
   return (
-    <>
-      <section className="overflow-hidden rounded-xl">
+    <section className="flex h-full flex-col">
+      <div className="h-full overflow-hidden rounded-xl">
         {/* Used div with onClick instead of Link to fix hydration error - using a button requires an outer div for styling - added an id for screen readers */}
         <div
           id="about-link"
-          className="group flex h-80 cursor-pointer flex-col justify-center  rounded-xl bg-[#172337] duration-300 hover:scale-110 hover:bg-[#1d2739] max-md:w-full"
+          className="group flex h-full cursor-pointer flex-col justify-center rounded-xl bg-[#172337] duration-300 hover:scale-110 hover:bg-[#1d2739] max-md:h-80 max-md:w-full"
           onClick={(event) => handleAboutClick(event)}
         >
           <div className="flex flex-col text-center text-4xl font-bold text-slate-400 duration-300 hover:text-red-300 group-hover:text-slate-200 group-hover:duration-300 max-sm:text-3xl">
@@ -36,15 +36,17 @@ const Contact = () => {
 
           <Socials isHomePage={true} />
         </div>
-      </section>
+      </div>
 
-      <h1 className="mt-3 text-wrap text-center text-sm font-medium uppercase text-slate-100">
-        About
-      </h1>
-      <h3 className="text-center text-sm font-light text-slate-300 opacity-80">
-        Background and interests
-      </h3>
-    </>
+      <div className="flex flex-col">
+        <h1 className="mt-3 text-wrap text-center text-sm font-medium uppercase text-slate-100">
+          About
+        </h1>
+        <h3 className="text-center text-sm font-light text-slate-300 opacity-80">
+          Background and interests
+        </h3>
+      </div>
+    </section>
   );
 };
 
