38331e47c94ee1a358ec57b6ffd76b2510ed8dee
DDVVPP <darshinvp@gmail.com>
2024-08-23 12:19:51 -0700

    refactor: added Footer component to remove duplicate code

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Home from '@/components/Home';
+import Footer from '@/components/shared/Footer';
 
 const HomeWrapper = () => {
   return (
@@ -6,9 +7,7 @@ const HomeWrapper = () => {
       <section className="w-full bg-slate-900">
         <Home />
       </section>
-      <footer className="mb-0 flex w-full justify-center bg-slate-900 py-5 pr-[51px] align-bottom text-xs font-light text-slate-400 max-md:justify-center max-md:text-wrap max-md:px-12 max-md:text-center">
-        © Designed & Built by Darshin Van Parijs & Deployed with Vercel
-      </footer>
+      <Footer isHomePage={true} />
     </>
   );
 };
