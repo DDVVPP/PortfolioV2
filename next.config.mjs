80144f4c39a4d3c3b6f5ff302766b9d750811bf6
DDVVPP <darshinvp@gmail.com>
2024-08-23 16:28:19 -0700

    update: added temporary redirect for project details

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/projects/dev-today',
+        destination: '/projects',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
