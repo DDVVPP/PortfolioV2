88b38a8e31f1c88c667d94ddbb52fb084b452898
DDVVPP <darshinvp@gmail.com>
2024-08-23 14:19:24 -0700

    fix: event propogation on div About click in Home Page issue

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import { useRouter } from 'next/navigation';
 
 import Socials from './shared/Socials';
@@ -7,13 +8,26 @@ import Socials from './shared/Socials';
 const Contact = () => {
   const router = useRouter();
 
+  const handleAboutClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    const div = event.currentTarget;
+    const links = div.querySelector('#socials');
+    if (links === event.target) {
+      return;
+    }
+
+    router.push('/about');
+  };
+
   return (
     <>
       <section className="overflow-hidden rounded-xl">
-        {/* Used div with onClick instead of Link to fix hydration error - using a button requires an outer div for styling */}
+        {/* Used div with onClick instead of Link to fix hydration error - using a button requires an outer div for styling - added an id for screen readers */}
         <div
+          id="about-link"
           className="group flex h-80 cursor-pointer flex-col justify-center  rounded-xl bg-[#172337] duration-300 hover:scale-110 hover:bg-[#1d2739] max-md:w-full"
-          onClick={() => router.push('/about')}
+          onClick={(event) => handleAboutClick(event)}
         >
           <div className="flex flex-col text-center text-4xl font-bold text-slate-400 duration-300 hover:text-red-300 group-hover:text-slate-200 group-hover:duration-300 max-sm:text-3xl">
             <h1>Darshin</h1>
--- components/shared/Socials.tsx
+++ components/shared/Socials.tsx
@@ -5,7 +5,7 @@ import linkedInLogo from '@/public/linkedin.svg';
 
 const Socials = ({ isHomePage }: { isHomePage: boolean }) => {
   return (
-    <div className="mt-0 flex flex-col gap-y-1">
+    <div id="socials" className="mt-0 flex flex-col gap-y-1">
       <div
         className={`mb-0.5 mt-2 flex items-center  gap-x-4 text-sm font-light text-slate-300 ${isHomePage ? 'justify-center' : 'navbar-mobile-info-links justify-end'}`}
       >
@@ -13,6 +13,7 @@ const Socials = ({ isHomePage }: { isHomePage: boolean }) => {
           href="https://www.linkedin.com/in/DarshinVanParijs/"
           target="_blank"
           className="hover:animate-[wiggle_0.3s_ease-in-out_infinite]"
+          onClick={(event) => event.stopPropagation()}
         >
           <Image priority src={linkedInLogo} alt="LogoLight" width={22} />
         </a>
@@ -20,6 +21,7 @@ const Socials = ({ isHomePage }: { isHomePage: boolean }) => {
           href="https://github.com/DDVVPP"
           target="_blank"
           className="hover:animate-[wiggle_0.3s_ease-in-out_infinite]"
+          onClick={(event) => event.stopPropagation()}
         >
           <Image priority src={githubLogo} alt="LogoLight" width={19} />
         </a>
@@ -27,6 +29,7 @@ const Socials = ({ isHomePage }: { isHomePage: boolean }) => {
           href="/resume-2024.pdf"
           target="_blank"
           className="rounded-md border border-slate-500 p-1 duration-300 hover:border-red-400 hover:text-slate-300 group-hover:text-slate-200 group-hover:duration-300 max-sm:text-xs"
+          onClick={(event) => event.stopPropagation()}
         >
           <p>Resume</p>
         </a>
@@ -36,6 +39,7 @@ const Socials = ({ isHomePage }: { isHomePage: boolean }) => {
         <a
           href="mailto:darshinvp@gmail.com"
           className="rounded-md border border-slate-500 p-1 text-end text-sm font-light text-slate-300 duration-300 hover:border-red-400 hover:text-slate-300 group-hover:text-slate-200 group-hover:duration-300 max-sm:text-xs"
+          onClick={(event) => event.stopPropagation()}
         >
           darshinvp@gmail.com
         </a>
