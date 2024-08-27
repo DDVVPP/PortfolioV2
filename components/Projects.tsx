8ec947eaf24e72424f9d03e91b549fae5e59210c
DDVVPP <darshinvp@gmail.com>
2024-08-26 20:56:20 -0700

    update: updated DevToday to be clickable

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image';
 
 import { projects } from '@/lib/constants';
 import Tag from './shared/Tag';
+import Link from 'next/link';
+import AngledArrow from './shared/AngledArrow';
 
 const Projects = () => {
   return (
@@ -15,7 +17,7 @@ const Projects = () => {
     >
       <div className="mx-10 my-32 flex flex-col gap-y-8 max-md:my-14">
         {projects.map((project) => {
-          return (
+          return project.isDisabled ? (
             <div
               key={project.title}
               className="flex animate-pulse gap-x-9 rounded-lg p-4 pl-3 max-md:flex-col max-md:gap-y-4 max-md:px-3"
@@ -44,6 +46,40 @@ const Projects = () => {
                 </div>
               </div>
             </div>
+          ) : (
+            <Link
+              key={project.title}
+              href={project.route}
+              className="group flex gap-x-9 rounded-lg p-4 pl-3 duration-300 hover:bg-slate-900/50 max-md:flex-col max-md:gap-y-4 max-md:px-3"
+            >
+              <div className="relative w-80 min-w-20 cursor-pointer max-md:w-full">
+                <Image
+                  alt={project.altText}
+                  src={project.coverImage}
+                  className="rounded-lg"
+                  placeholder="blur"
+                />
+              </div>
+              <div className="mb-3 flex w-3/4 max-w-[1000px] flex-col justify-between max-md:-order-1 max-md:mb-2 max-md:w-full">
+                <div className="flex gap-x-0.5 max-md:justify-center">
+                  <h1 className="text-wrap text-sm font-medium uppercase text-slate-100 duration-300 group-hover:text-corral">
+                    {project.title}
+                  </h1>
+                  <AngledArrow
+                    alt="arrow-up-right"
+                    className="flex stroke-slate-400 duration-300 group-hover:-translate-y-0.5 group-hover:translate-x-0.5"
+                  />
+                </div>
+                <h3 className="text-sm font-light text-slate-300 max-md:text-center">
+                  {project.description}
+                </h3>
+                <div className="mt-4 flex flex-wrap gap-x-3 gap-y-2 max-md:justify-center">
+                  {project.tags.map((tag) => {
+                    return <Tag key={tag} text={tag} />;
+                  })}
+                </div>
+              </div>
+            </Link>
           );
         })}
       </div>
