39c230d6ac305f18326c49b9e81468dcf7028c78
DDVVPP <darshinvp@gmail.com>
2024-09-02 11:57:12 -0700

    fix: LinkWithIcon component to a tag and target blank

--- components/shared/LinkWithIcon.tsx
+++ components/shared/LinkWithIcon.tsx
@@ -1,16 +1,16 @@
-import Link from 'next/link';
 import { ExternalLinkIcon } from 'lucide-react';
 
 const LinkWithIcon = ({ label, href }: { label: string; href: string }) => {
   return (
-    <Link
+    <a
       href={href}
+      target="_blank"
       className="flex w-fit items-center gap-x-1 rounded-md border border-slate-500 px-2 py-1 text-sm font-light  text-slate-300 duration-300 hover:border-red-400 hover:text-slate-300 group-hover:text-slate-200 group-hover:duration-300 max-sm:py-0.5 max-sm:text-[11px]"
     >
       <p>{label}</p>
 
       <ExternalLinkIcon className="mb-0.5 flex size-4 items-center stroke-slate-200 stroke-1 max-sm:size-3" />
-    </Link>
+    </a>
   );
 };
 
