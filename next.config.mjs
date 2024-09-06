a1f8776a1a2b6665cbbcad8d162590908507c5f0
DDVVPP <darshinvp@gmail.com>
2024-09-06 13:13:23 -0700

    fix: removed redirect in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/projects/dev-today',
-  //       destination: '/projects',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
-};
+const nextConfig = {};
 
 export default nextConfig;
