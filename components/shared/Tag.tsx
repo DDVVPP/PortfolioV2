88fb323c5bd7b57d1a0c3819e950b44ccb58d557
DDVVPP <darshinvp@gmail.com>
2024-12-06 15:31:43 -0800

    style: added textSize and pillWidth prop to Tag component

--- components/shared/Tag.tsx
+++ components/shared/Tag.tsx
@@ -1,15 +1,19 @@
 const Tag = ({
   text,
   textColor = 'text-slate-400',
+  textSize = 'text-[12px] max-sm:text-[10px]',
   borderColor = 'border-slate-400',
+  pillWidth,
 }: {
   text: string;
   textColor?: string;
+  textSize?: string;
   borderColor?: string;
+  pillWidth?: string;
 }) => {
   return (
     <p
-      className={`text-nowrap rounded-2xl border ${borderColor} px-2 pt-0.5 text-[12px] ${textColor} h-fit duration-300 group-hover:border-red-400 max-sm:text-[10px]`}
+      className={`text-nowrap rounded-2xl border ${borderColor} px-2 py-0.5 ${textColor} h-fit duration-300 group-hover:border-red-400 ${textSize} ${pillWidth}`}
     >
       {text}
     </p>
