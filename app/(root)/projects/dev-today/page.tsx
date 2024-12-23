d746b593a4d6b9773a6a286fbb69938858bc3221
DDVVPP <darshinvp@gmail.com>
2024-12-22 21:04:44 -0800

    cleanup: deleted ProjectDetails and route

--- app/(root)/projects/dev-today/page.tsx
+++
@@ -1,8 +0,0 @@
-import ProjectDetails from '@/components/ProjectDetails';
-import { projects } from '@/lib/constants';
-
-const DevTodayWrapper = () => {
-  return <ProjectDetails project={projects[0]} />;
-};
-
-export default DevTodayWrapper;
