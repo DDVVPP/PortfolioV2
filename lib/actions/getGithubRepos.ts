fe10d161f00e7b88a1f1ffe09d49f7fa2610539e
DDVVPP <darshinvp@gmail.com>
2025-03-12 17:09:06 -0700

    cleanup: added comments and refactored/cleaned up code

--- lib/actions/getGithubRepos.ts
+++ lib/actions/getGithubRepos.ts
@@ -44,23 +44,22 @@ export const getGithubRepos = async () => {
 };
 
 const getThisYearsPublicRepoNames = async (year: string) => {
-  const response = await fetch(
-    'https://api.github.com/users/DDVVPP/repos?visibility=public'
-  );
-  const data = await response.json();
-  const reposUpdatedThisYear = data.filter((repo: Repo) => {
-    const updatedAtYear = repo.updated_at.split('-')[0];
-    return updatedAtYear === year;
-  });
-  const repoNames = reposUpdatedThisYear
-    .filter(
-      (repo: Repo) => !['DDVVPP', 'HackForLA-Website'].includes(repo.name)
-    )
-    .map((repo: Repo) => {
-      return repo.name;
-    });
+  try {
+    const response = await fetch(
+      'https://api.github.com/users/DDVVPP/repos?visibility=public'
+    );
+    const data = await response.json();
 
-  return repoNames;
+    // Return the name of repos that were updated this year
+    const repoNames = data
+      .filter((repo: Repo) => repo.updated_at.startsWith(year))
+      .map((repo: Repo) => repo.name);
+
+    return repoNames;
+  } catch (error) {
+    console.error('Error fetching repoNames from GitHub:', error);
+    return { error: 'An unexpected error occurred while fetching repoNames' };
+  }
 };
 
 const getRepoPRs = async (
@@ -79,10 +78,7 @@ const getRepoPRs = async (
 
     // Find repos that are updated this year and sort them by most recent
     const repoPRsUpdatedThisYear = data
-      .filter((repo: Repo) => {
-        const updatedAtYear = repo.updated_at.split('-')[0];
-        return updatedAtYear === year;
-      })
+      .filter((repo: Repo) => repo.updated_at.startsWith(year))
       .sort((a: Repo, b: Repo) => b.updated_at.localeCompare(a.updated_at));
 
     // Return the n most recent repos with the specified object properties
@@ -119,15 +115,15 @@ const getRepoCommits = async (
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
+
+    // Find repos that are updated this year and sort them by most recent
     const repoCommitsUpdatedThisYear = data
-      .filter((repo: Repo) => {
-        const updatedAtYear = repo.commit.author.date.split('-')[0];
-        return updatedAtYear === year;
-      })
+      .filter((repo: Repo) => repo.commit.author.date.startsWith(year))
       .sort((a: Repo, b: Repo) =>
         b.commit.author.date.localeCompare(a.commit.author.date)
       );
 
+    // Return the n most recent repos with the specified object properties
     const filteredData = repoCommitsUpdatedThisYear
       .slice(0, numberOfCommits)
       .map((repo: Repo) => {
