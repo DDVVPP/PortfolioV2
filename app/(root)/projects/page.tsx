1719ca6013a2ef70a87330d1538b41077b8fd164
DDVVPP <darshinvp@gmail.com>
2025-03-14 13:03:17 -0700

    wip: exposed repoPrs and commits to FE

--- app/(root)/projects/page.tsx
+++ app/(root)/projects/page.tsx
@@ -1,7 +1,12 @@
 import Projects from '@/components/Projects';
+import { getGithubPRsAndCommits } from '@/lib/actions/getGithubPRsAndCommits';
 
 const ProjectsWrapper = async () => {
-  return <Projects />;
+  const { repoPRs, repoCommits } = await getGithubPRsAndCommits();
+  if (!repoPRs) return null;
+  if (!repoCommits) return null;
+
+  return <Projects repoPRs={repoPRs} repoCommits={repoCommits} />;
 };
 
 export default ProjectsWrapper;
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,18 +4,25 @@ import { motion } from 'framer-motion';
 
 import { projects } from '@/lib/constants';
 import ProjectCard from './ProjectCard';
+import { RepoCommit, RepoPR } from '@/lib/types';
 
-const Projects = () => {
+const Projects = ({
+  repoPRs,
+  repoCommits,
+}: {
+  repoPRs: RepoPR[];
+  repoCommits: RepoCommit[];
+}) => {
   return (
     <motion.section
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.8 }}
-      className='mx-7 my-32 flex flex-col gap-y-8 max-md:my-12 max-md:gap-y-10'
+      className="mx-7 my-32 flex flex-col gap-y-8 max-md:my-12 max-md:gap-y-10"
     >
-        {projects.map((project) => (
-          <ProjectCard project={project} key={project.altText} />
-        ))}
+      {projects.map((project) => (
+        <ProjectCard project={project} key={project.altText} />
+      ))}
     </motion.section>
   );
 };
