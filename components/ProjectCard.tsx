b69f10172f6ad4c80e5e59db6cfed577d92e682a
DDVVPP <darshinvp@gmail.com>
2025-03-17 19:32:05 -0700

    wip: added accordion component

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,6 +2,12 @@ import Image from 'next/image';
 import Tag from './shared/Tag';
 import LinkWithIcon from './shared/LinkWithIcon';
 import { Project, RepoCommit, RepoPR } from '@/lib/types';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 const ProjectCard = ({
   project,
@@ -60,6 +66,15 @@ const ProjectCard = ({
             return <Tag key={tag} text={tag} />;
           })}
         </div>
+
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
     </section>
   );
