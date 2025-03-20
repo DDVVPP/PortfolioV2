3c601da8a69972bde6856d48b413bd1fb6a1bfad
DDVVPP <darshinvp@gmail.com>
2025-03-19 19:02:02 -0700

    refactor: pulled out no commits or prs div into util function at top

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -23,6 +23,16 @@ const ProjectCard = ({
   pullRequests: RepoPR[];
   commits: RepoCommit[];
 }) => {
+  const renderNoCommitsOrNoPRsDiv = (type: string) => {
+    return (
+      <div className="rounded-md bg-dark-800/65 border border-dark-800 p-4">
+        <p className="text-sm font-light text-slate-300 max-sm:text-[13px] 3xl:text-base">
+          No {type} created this year
+        </p>
+      </div>
+    );
+  };
+
   return (
     <section className="group flex gap-x-9 gap-y-4 rounded-lg bg-slate-900/50 p-6 pl-3 max-md:flex-col max-md:gap-y-3">
       <div className="relative ml-4 w-80 min-w-20 content-center pt-1 max-md:mx-0  max-md:mt-1 max-md:w-full">
@@ -75,21 +85,13 @@ const ProjectCard = ({
         {pullRequests.length > 0 ? (
           <PullRequestsAccordion pullRequests={pullRequests} />
         ) : (
-          <div className="rounded-md bg-dark-800/65 border border-dark-800 p-4">
-            <p className="text-sm font-light text-slate-300 max-sm:text-[13px] 3xl:text-base">
-              No pull requests created this year
-            </p>
-          </div>
+          renderNoCommitsOrNoPRsDiv('pull requests')
         )}
 
         {commits.length > 0 ? (
           <CommitsAccordion commits={commits} />
         ) : (
-          <div className="rounded-md bg-dark-800/65 border border-dark-800 p-4">
-            <p className="text-sm font-light text-slate-300 max-sm:text-[13px] 3xl:text-base">
-              No commits created this year
-            </p>
-          </div>
+          renderNoCommitsOrNoPRsDiv('commits')
         )}
       </div>
     </section>
